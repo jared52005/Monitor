fb3a20c29cb7209ac71c1e6bd13e73ab58c34cbf
Jaromir Mayer <>
2020-09-08 22:02:37 +0100

    UDS - Added 04, 11, 14, 2E, 34, 36 parsing

--- Plugins/UDS.lua
+++ Plugins/UDS.lua
@@ -1,11 +1,12 @@
 -- LUA bitwise operations: https://www.lua.org/manual/5.2/manual.html#6.7
 -- constants
 local sid_dict_description = {
+    [0x04] = "Clear DTC (Emission related ECUs)",
     [0x10] = "Start Diagnostic Session",
     [0x11] = "ECU Reset",
     [0x12] = "Read Freeze Frame Data",
     [0x13] = "Read Diagnostic Trouble Code",
-    [0x14] = "Clear Diagnostic Information",
+    [0x14] = "Clear DTC (Standard method)",
     [0x17] = "Read Status of DTCs",
     [0x18] = "Read DTCs by Status",
     [0x22] = "RDBLI",
@@ -49,6 +50,24 @@ local nrc_description = {
     [0x7F] = "Service or Subfunction not supported",
 }
 
+function NegativeResponse_InfoColumn(tvbuf, pktinfo)
+    -- `[7F2233] - RDBLI; Security Requested` = Service ID (22 = RDBLI) Negative Response code (33 = Security)
+    local preview = tostring(tvbuf:range(0,3))
+    local nrc = nrc_description[tvbuf:range(2,1):uint()]
+    local sid = tvbuf:range(1,1):uint()
+    if nrc == nil then
+        nrc= "???"
+    end
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[sid] .. "; " .. nrc
+end
+
+-- UDS 14
+function EDTC_PID_InfoColumn(tvbuf, pktinfo)
+    -- `[14] - Erase DTC`
+    local preview = tostring(tvbuf:range(0,1))
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x04]
+end
+
 -- UDS 10
 local sds_dict_description = {
     ["01"] = "Default",
@@ -65,9 +84,32 @@ function SDS_InfoColumn(tvbuf, pktinfo)
     pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x10] .. "; " .. type
 end
 
+-- UDS 11
+local erst_dict_description = {
+    ["01"] = "Hard reset",
+}
+function ERST_InfoColumn(tvbuf, pktinfo)
+    -- `[1101] - Start Diagnostic Session; Default`
+    local preview = tostring(tvbuf:range(0,2))
+    local type = erst_dict_description[tostring(tvbuf:range(1,1))]
+    if type == nil then
+        type= "???"
+    end
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x10] .. "; " .. type
+end
+
+-- UDS 14
+function EDTC_InfoColumn(tvbuf, pktinfo)
+    -- `[14] - Erase DTC`
+    local preview = tostring(tvbuf:range(0,4))
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x14]
+end
+
 --UDS 22
 local rdbli_dict_description = {
     ["f190"] = "VIN",
+    ["f189"] = "Software Revision",
+    ["f187"] = "Software Type",
 }
 
 function RDBLI_InfoColumn(tvbuf, pktinfo)
@@ -80,9 +122,34 @@ function RDBLI_InfoColumn(tvbuf, pktinfo)
     pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x22] .. "; " .. type
 end
 
+--UDS 27
+function SA_InfoColumn(tvbuf, pktinfo)
+    -- `[2701] - Seceurity access`
+    local preview = tostring(tvbuf:range(0,2))
+    local type = "Level: " .. tostring(tvbuf:range(1,1))
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x27] .. "; " .. type
+end
+
+--UDS 2E
+local wdbli_dict_description = {
+    ["f15a"] = "Progamming Fingerprints",
+}
+
+function WDBLI_InfoColumn(tvbuf, pktinfo)
+    -- `[22F190] - RDBLI; VIN` = First 3 bytes of request, Service ID (22 = RDBLI) Subfunciton ID (F190 = VIN)
+    local preview = tostring(tvbuf:range(0,3))
+    local type = wdbli_dict_description[tostring(tvbuf:range(1,2))]
+    if type == nil then
+        type= "???"
+    end
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x22] .. "; " .. type
+end
+
 --UDS 31
 local rc_dict_description = {
     ["0203"] = "Programming Preconditions",
+    ["ff00"] = "Erase",
+    ["ff01"] = "Checksum",
 }
 function RC_InfoColumn(tvbuf, pktinfo)
     -- `[31010203] - Tester Present; Default`
@@ -92,6 +159,41 @@ function RC_InfoColumn(tvbuf, pktinfo)
         type= "???"
     end
     pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x31] .. "; " .. type
+end
+
+--UDS 34
+function RD_InfoColumn(tvbuf, pktinfo)
+    sid = tvbuf:range(0,1):uint()
+    if(sid == 0x34 ) then
+        -- `[34 1e c0 00 01 01 3e 00] - Request download <address> [<length>]`
+        local preview = tostring(tvbuf:range(0,1))
+
+        local fmt = tvbuf:range(2,1):uint()
+        local fmt_length = bit32.rshift(bit32.band(fmt, 0xF0), 4)
+        local fmt_address = bit32.band(fmt, 0xF)
+
+        local address = tostring(tvbuf:range(3, fmt_address))
+        local length = tostring(tvbuf:range(3 + fmt_address,fmt_length))
+        pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x34] .. "; " .. string.upper(address) .. " [" .. string.upper(length) .. "]"
+    else
+        local preview = tostring(tvbuf:range(0,1))
+        pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x34]
+    end
+end
+
+--UDS 36
+function T_InfoColumn(tvbuf, pktinfo)
+    -- `[36] - Data transfer
+    local preview = tostring(tvbuf:range(0,1))
+    local seq = tostring(tvbuf:range(1,1))
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "...] - " ..  sid_dict_description[0x36] .. "; SEQ = " .. seq
+end
+
+--KWP 37
+function RTE_InfoColumn(tvbuf, pktinfo)
+    -- `[37] - Request transfer exit
+    local preview = tostring(tvbuf:range(0,1))
+    pktinfo.cols.info = "[" .. string.upper(preview) .. "] - " ..  sid_dict_description[0x37]
 end
 
 --UDS 3E
@@ -111,9 +213,17 @@ function TP_InfoColumn(tvbuf, pktinfo)
 end
 
 local sid_dict_methods = {
+    [0x04] = EDTC_PID_InfoColumn,
     [0x10] = SDS_InfoColumn,
+    [0x11] = ERST_InfoColumn,
+    [0x14] = EDTC_InfoColumn,
     [0x22] = RDBLI_InfoColumn,
+    [0x27] = SA_InfoColumn,
+    [0x2E] = WDBLI_InfoColumn,
     [0x31] = RC_InfoColumn,
+    [0x34] = RD_InfoColumn,
+    [0x36] = T_InfoColumn,
+    [0x37] = RTE_InfoColumn,
     [0x3E] = TP_InfoColumn,
 }
 
@@ -140,8 +250,6 @@ uds_dissector.fields = {
     data,
 }
 
-pktState = {}
-
 function uds_dissector.dissector(tvbuf,pktinfo,root)
     -- set the protocol column to show our protocol name
     pktinfo.cols.protocol:set("UDS")
@@ -157,7 +265,7 @@ function uds_dissector.dissector(tvbuf,pktinfo,root)
         tree:add(sid_isneg, tvbuf:range(0,1))
         tree:add(sid, tvbuf:range(1,1))
         tree:add(nrc, tvbuf:range(2,1))
-        pktinfo.cols.info = "NOK"
+        NegativeResponse_InfoColumn(tvbuf, pktinfo)
     --Is positive response? (SID | 0x40)
     elseif bit32.btest(cur_sid, positive_response_mask) then
         tree:add(sid_ispos, tvbuf:range(0,1))
