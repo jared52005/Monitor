2a692bb09ac4f8bd5041f475921e9b01e4d7c6d8
Jaromir Mayer <>
2023-10-06 21:27:01 +0300

    CanDriver - Implemented Start, Stop, basic queues

--- Firmware/OlimexP405/BSW/CanDriver.c
+++ Firmware/OlimexP405/BSW/CanDriver.c
@@ -12,6 +12,9 @@
 #include "rtos_utils.h"
 //******************************************************************************
 
+static QueueHandle_t canMessage_RxQueue = NULL;
+static QueueHandle_t canMessage_TxQueue = NULL;
+static bool _stop;
 /**
 * @brief  Thread in which we are refreshing CAN state machine
 */
@@ -19,25 +22,88 @@ void CanDriver_Task(void* arg)
 {
 	ErrorCodes canResult;
 	CanMessage canRxMsg;
-	int i;
-    for(;;)
+	CanMessage canTxMsg;
+	_stop = false;
+	
+	printf("CAN Driver task has started\n");
+    if (Can_Enable() != ERROR_OK)
+	{
+        printf("ERROR: Init of CAN peripheral has failed\r\n");
+    }
+	//Hold 32 messages max 
+  	canMessage_RxQueue = xQueueCreate( 32, sizeof( CanMessage* ) );
+	canMessage_TxQueue = xQueueCreate( 32, sizeof( CanMessage* ) );
+    while(_stop == false)
     {
+		//Check RX messages
         canResult = Can_Rx(&canRxMsg);
 	    if (canResult == ERROR_OK)
-	    {  
-			printf("CAN Raw - [");
-			for(i = 0; i < canRxMsg.Dlc; i++)
-			{
-				printf("%x ", canRxMsg.Frame[i]);
-			}
-			printf("]\r\n");
-			canRxMsg.Id = 0x7FF;
-		    Can_Tx(&canRxMsg);
+	    {
+			//print received CAN message on USB
+			printf("Received CAN data\n");
+
 	    }
-		if(canResult == ERROR_DATA_OVERFLOW)
-		{
-			printf("CAN Raw - Overflow!\r\n");
-		}
+		
+		//Check TX Queue
+    	if(xQueueReceive( canMessage_RxQueue, &(canTxMsg), ( TickType_t ) 0 ) == pdPASS)
+    	{
+			//Copy allocated xcmsg into user's cmsg, and free it
+			Can_Tx(&canTxMsg);
+      		vPortFree(&canTxMsg);
+    	}
 		taskYIELD();
     }
+	vQueueDelete(canMessage_RxQueue);
+	vQueueDelete(canMessage_TxQueue);
+	vTaskDelete(NULL);
+}
+
+void CanDriver_Start(void)
+{
+	xTaskCreate(CanDriver_Task, (const char*)"CAN Driver", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+}
+
+void CanDriver_Stop(void)
+{
+	_stop = true;
+}
+
+void CanDriver_Transmit(CanMessage cmsg)
+{
+	CanMessage* qCanMessage;
+	if(canMessage_TxQueue == NULL)
+	{
+    	return;
+  	}
+	//Copy cmsg into allocated qCanMessage and queue it
+    qCanMessage = (CanMessage*)pvPortMalloc(sizeof(CanMessage));
+    qCanMessage->Dlc = cmsg.Dlc;
+    qCanMessage->Id = cmsg.Id;
+    qCanMessage->Timestamp = cmsg.Timestamp;
+    memcpy(qCanMessage->Frame, cmsg.Frame, cmsg.Dlc);
+    xQueueSend(canMessage_TxQueue, ( void * ) &qCanMessage, (TickType_t)0);
+}
+
+bool CanDriver_Receive(CanMessage* cmsg)
+{
+	CanMessage* xcmsg;
+	if(canMessage_TxQueue == NULL)
+	{
+    	return false;
+  	}
+
+    if(xQueueReceive( canMessage_RxQueue, &(xcmsg), ( TickType_t ) 0 ) == pdPASS)
+    {
+		//Copy allocated xcmsg into user's cmsg, and free it
+		cmsg->Dlc = xcmsg->Dlc;
+    	cmsg->Id = xcmsg->Id;
+    	cmsg->Timestamp = xcmsg->Timestamp;
+    	memcpy(cmsg->Frame, xcmsg->Frame, cmsg->Dlc);
+      	vPortFree(xcmsg);
+	  	return true;
+    }
+	else
+	{
+		return false;
+	}
 }
--- Firmware/OlimexP405/BSW/CanDriver.h
+++ Firmware/OlimexP405/BSW/CanDriver.h
@@ -1,7 +1,25 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include "CanIf.h"
 
 /**
 * @brief  Thread in which we are refreshing CAN state machine
 */
 void CanDriver_Task(void* arg);
+/**
+ * @brief Enqueue data for transmission
+ */
+void CanDriver_Transmit(CanMessage cmsg);
+/**
+ * @brief Get enqueued data.
+ * @return True if message was dequeued. False if there is no message available.
+ */
+bool CanDriver_Receive(CanMessage* cmsg);
+/**
+ * @brief Start CAN driver task
+*/
+void CanDriver_Start(void);
+/**
+ * @brief Stop CAN driver task
+*/
+void CanDriver_Stop(void);
